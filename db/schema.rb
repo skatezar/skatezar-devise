b0dd7834991357428abbafa9997042d6becd925c
Laurenz Gerold <skatezar@gmail.com>
2021-02-22 20:28:59 +0100

    navbar implemented with login if else logic of devise, styling not done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_184950) do
+ActiveRecord::Schema.define(version: 2021_02_22_190238) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "category"
+    t.string "brand"
+    t.string "size"
+    t.string "condition"
+    t.string "location"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_02_22_184950) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
